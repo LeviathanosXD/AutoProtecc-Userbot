814c18b473129c4e33943d4b92696a5e55e5c631
Lyfe Sredrum <95935447+LeviathanosXD@users.noreply.github.com>
2022-03-15 21:50:07 -0400

    Update __init__.py

--- AutoProteccUserbot/__init__.py
+++ AutoProteccUserbot/__init__.py
@@ -10,6 +10,4 @@ DELAY = os.environ.get("DELAY", None)
 BOT_LIST = {int(x) for x in os.environ.get("BOT_LIST", "").split()}
 
 if STRING_SESSION:
-    Waifu = Client(Session(STRING_SESSION), api_id=API_ID, api_hash=API_HASH)
-else:
     Waifu = Client("userbot", api_id=API_ID, api_hash=API_HASH)
